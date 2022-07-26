dd85dd09de1c8ca7e5891db7a8152c6772e52810
manabutice <manabutice@gmail.com>
2022-07-26 18:55:02 +0900

    config.hosts << "morning-inlet-28724.herokuapp.com" 追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "morning-inlet-28724.herokuapp.com"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
